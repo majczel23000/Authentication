cb8b047f1406bb8f1048598957e5061ef37be910
majczel23000 <michal.razny23@gmail.com>
2018-07-11 09:38:39 +0200

    new update

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +8,16 @@ import { ApiService } from '../api.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(public apiService: ApiService) { }
+  constructor(public apiService: ApiService, private _router: Router) { }
 
   ngOnInit() {
+  }
+
+  navbarBrandClick(){
+    if(this.apiService.loggeddIn())
+      this._router.navigate(['/dashboard']);
+    else
+      this._router.navigate(['/login']);
   }
 
 }
